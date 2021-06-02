f446bc92e78de00661d8998021d565ce5f804a19
Kevin Blair <kblair40@gmail.com>
2021-06-02 15:29:22 -0500

    Add dummy orders and Cart component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Food Order App</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of food" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
